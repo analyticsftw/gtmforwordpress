ce1f3aa593a803858dad9ba52bd43c38b3f9fd68
Julien Coquet <julien.coquet@gmail.com>
2016-07-18 22:54:43 +0200

    Added support for login status

--- functions.php
+++ functions.php
@@ -18,60 +18,49 @@
 
 function dataLayer()
 {
-	$gtmBuffer = ''; // initialize GTM dataLayer output
-	$gtmBuffer.= "\n
+  $gtmBuffer = ''; // initialize GTM dataLayer output
+  $gtmBuffer.= "\n
 <!-- Google Tag Manager Data Layer -->
 <script type='text/javascript'>
-
   // URL JavaScript toolbox, saves times creating variables
   var _d = document;
   var _dl = _d.location;
   var _dlp = _dl.pathname;
   var _dls = _dl.search;
   var _dr = _d.referrer;
-  var dataLayer = [{
-";
-	if (is_404()) { // Error page management
-		$gtmBuffer.= "\n
-    'is404': '" . is_404() . "', // Is this a 404 page? (boolean)
-    'url404': '/404' + _dlp + '|' + (_dr ? _dr : 'direct'), // If 404, create virtual URL with referrer
-";
+  var dataLayer = [{";
+  $gtmBuffer.= "\n\t'isLoggedIn': '" . (is_user_logged_in()==true ?  'Yes' : 'No')."',"; 
+  // Is this a 404 page? (boolean)		
+  if (is_404()) { // Error page management
+	$gtmBuffer.= "\n\t'is404': '" . is_404() . "', // Is this a 404 page? (boolean)
+    'url404': '/404' + _dlp + '|' + (_dr ? _dr : 'direct'), // If 404, create virtual URL with referrer";
+  }
+  if (is_home()) { // is this the home page
+	$gtmBuffer.= "\n\t'postType': 'home',";
+  }
+  if (is_single() || is_page()) { // in case of page or post
+	// Get the page/post category. In case of multiple categories, create an array
+	$gtm_cat = get_the_category();
+	for ($cati = 0; $cati < count($gtm_cat); $cati++) {
+		$gtm_cats[] = $gtm_cat[$cati]->cat_name;
 	}
-
-	if (is_home()) { // is this the home page
-		$gtmBuffer.= "\n
-    'postType': 'home',
-";
+	// Walk through post tags
+	$gtm_cats = trim($gtm_cats);	 
+	$posttags = get_the_tags();
+	if ($posttags) {
+		foreach($posttags as $tag) {$gtm_tags.= $tag->name . ' ';}
 	}
-
-	if (is_single() || is_page()) { // in case of page or post
-		// Get the page/post category. In case of multiple categories, create an array
-		$gtm_cat = get_the_category();
-		for ($cati = 0; $cati < count($gtm_cat); $cati++) {
-			$gtm_cats[] = $gtm_cat[$cati]->cat_name;
-		}
-
-		// Walk through post tags
-		$gtm_cats = trim($gtm_cats);	 
-		$posttags = get_the_tags();
-		if ($posttags) {
-			foreach($posttags as $tag) {
-				$gtm_tags.= $tag->name . ' ';
-			}
-		}
-
-		$gtmBuffer.= "\n
+	$gtmBuffer.= "\n
     'author': '" . get_the_author() . "',
     'postType': '" . get_post_type() . "',
     'postCategory': '" . $gtm_cat[0]->cat_name . "',
     'postCategories': '".join(",",$gtm_cats)."',
     'postTags': '" . addslashes(trim($gtm_tags)) . "',";
-	}
-	$gtmBuffer.= "\n'terminator': 'I\'ll be back' // Always terminate your arrays!
+  }
+  $gtmBuffer.= "\n'terminator': 'I\'ll be back' // Always terminate your arrays!
   }];
-
 </script>\n";
-	print $gtmBuffer;
+  print $gtmBuffer;
 }
 
 // Tell Wordpress to insert the datalayer and append it to the HEAD tag
